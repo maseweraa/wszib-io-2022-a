e419706a0b670301532b4f925e779c777e687cdd
Marcel Seweryn <masewera@artemis.wszib.edu.pl>
2022-06-18 23:50:33 +0200

    Junior tester: pierwszy commit! :)

--- trojkaty.java
+++ trojkaty.java
@@ -13,8 +13,7 @@ class Trojkaty {
          if (a == b && b == c && a == c) {
              System.out.println("Trójkąt równoboczny"); 
          }
-         // TODO: tutaj trzeba bedzie dopisac inne przypadki
-    }
+     }
     /** Wyświetla ekran pomocy */
     public static void pomoc(){
         System.out.println("Acme INC. (C) 2022");
@@ -30,7 +29,12 @@ class Trojkaty {
         float a = Float.valueOf(args[0]);
         float b = Float.valueOf(args[1]);
         float c = Float.valueOf(args[2]);
-        
+
+        if (a < 0 || b < 0 || c < 0) {
+            System.out.println("Długości boków trójkąta muszą być nieujemne!");
+            System.exit(2);
+        }
+
         jakiTrojkat(a, b, c);
     }
 }
